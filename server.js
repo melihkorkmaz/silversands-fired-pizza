33010360774ad2dbfd3bb43d5e3d2e33e0a6aaa1
Melih Korkmaz <mk@count.ly>
2018-07-10 10:01:08 +0300

    Missing images

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
 var bodyParser = require('body-parser');
 var path = require('path');
 
@@ -11,9 +12,38 @@ app.set('view engine', 'ejs');
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_USER_PASS
+    }
+});
+
+app.post('/contact-form', (req, res) => {
+    var mailOptions = {
+        from: process.env.MAIL_USER,
+        to: 'melih@tdsmaker.com',
+        subject: 'Fired Pizza Contact Form',
+        html: `<div><b>Name:</b> ${req.body.firstName} ${req.body.lastName}</div>`
+        + `<div><b>Email:</b> ${req.body.email}</div>`
+        + `<div><b>Phone:</b> ${req.body.phone}</div>`
+        + `<div><b>Message:</b> ${req.body.message}</div>`
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            res.send({status : false, message : error.message});
+        } else {
+            res.send({status : true, message : "We've got your e-mail. Thank you!"});
+        }
+    });
+})
+
 var port = process.env.PORT || 8080;
-app.listen(port, () => { console.log('web-site has been started at 8080')});
+app.listen(port, () => { console.log('web-site has been started at 8080') });
